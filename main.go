d69ab5ec79a089c0e564fe0c8fbd658fb364dd53
yan adhi wiranata <yanadhiwiranata@gmail.com>
2024-03-03 16:03:27 +0700

    Add go chi router

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/yanadhiwiranata/go-todo/todo"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+
+	r.Mount("/todo", todo.TodosResource{}.Routes())
+	http.ListenAndServe(":3000", r)
+}
